ec991d4540a4d0cd76de598ceaa7f69f17e6285f
WennbergIT19A <70195299+WennbergIT19A@users.noreply.github.com>
2020-09-08 09:12:26 +0200

    öööh komit

--- Programering 1/Variabler_IO/index.js
+++ Programering 1/Variabler_IO/index.js
@@ -1,4 +1,12 @@
 // En variabel som heter name och tilldelar värdet "Wennberg"
-name = "Wennberg"
+let name = "Wennberg"
 
 console.log(`Hej ${name}`)
+
+//skapa variablern tal1, tal2, tal3
+//tilldela variabler med värden 3, 4, 5
+//Räkna ut medelvärdet av dem
+
+let tal1 = "3"
+let tal2 = "4"
+let tal3 = "5"
